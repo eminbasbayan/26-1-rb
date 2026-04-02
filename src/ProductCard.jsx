b72656456d402c75b9a851851e07ac7a34ec6df9
eminbasbayan <emin.basbayan@icloud.com>
2026-04-02 22:53:55 +0300

    component inline css

--- src/ProductCard.jsx
+++ src/ProductCard.jsx
@@ -1,6 +1,9 @@
 function ProductCard() {
   return (
-    <div className="product-card">
+    <div className="product-card" style={{
+        backgroundColor: "red",
+        color: "white"
+    }}>
       <img src="https://karatayberraksu.com.tr/wp-content/uploads/2019/03/5lt-buyuk.png" alt="Image" />
       <strong>Büyük Su</strong>
       <span>20₺</span>
