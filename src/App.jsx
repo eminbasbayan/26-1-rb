c8d4618192832a76222cadc9ab07d3e404df3c5a
eminbasbayan <emin.basbayan@icloud.com>
2026-04-02 22:10:26 +0300

    useState hook mantığı

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
-import { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 function App() {
+  // let myTitle = 'Ahmet Demir';
+  const [state, setState] = useState('Ahmet Demir')
+
+  function handleTitleChange() {
+    console.log("run!");
+    // myTitle = 'Emin Başbayan';
+    setState('Emin Başbayan')
+    // console.log(myTitle);
+  }
+
   return (
     <Fragment>
-      <h1>Hello React!</h1>
-      <button>İsmi Değiştir</button>
+      <h1>{state}</h1>
+      <button onClick={handleTitleChange}>İsmi Değiştir</button>
     </Fragment>
   );
 }
 
 export default App;
-
-/*     <>
-      <h1>Hello React!</h1>
-      <button>İsmi Değiştir</button>
-    </> */
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,4 @@
-import { createRoot } from "react-dom/client";
-import App, {myArray, myArray2, myArray3} from "./App";
+import { createRoot } from 'react-dom/client';
+import App from './App';
 
-
-createRoot(document.getElementById("root")).render(<App />)
+createRoot(document.getElementById('root')).render(<App />);
