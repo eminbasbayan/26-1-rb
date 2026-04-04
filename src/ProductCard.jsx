82bfe6e9b3aafb2ec4c8ca76ad39448a5670c3ef
eminbasbayan <emin.basbayan@icloud.com>
2026-04-04 22:48:46 +0300

    map kullanımı

--- src/ProductCard.jsx
+++ src/ProductCard.jsx
@@ -3,16 +3,16 @@ import './ProductCard.css';
 // component'in ilk parametresi sana bir obje {} verir. sen bunu
 // istediğin isimle adlandırabilirsin
 function ProductCard(props) {
-  const { myImage, title, price } = props;
+  const { myImage, title, price, desc } = props;
   console.log(props);
 
   return (
     <div className="product-card">
-      <span>{props.fullName}</span>
-      <img className="product-image" src={myImage} alt="Image" />
+      {/*  <img className="product-image" src={myImage} alt="Image" /> */}
       <div className="product-info">
         <strong className="product-title">{title}</strong>
         <span className="product-price">{price}₺</span>
+        <p className="product-description">{desc}</p>
       </div>
     </div>
   );
--- src/Products.jsx
+++ src/Products.jsx
@@ -1,24 +1,28 @@
-import ProductCard from './ProductCard';
+import ProductCard from './ProductCard.jsx';
+import productsData from './productsData';
 import './Products.css';
 
 // Ürünlerle ilgili ana parent component
 function Products() {
-  const image =
-    'https://karatayberraksu.com.tr/wp-content/uploads/2019/03/5lt-buyuk.png';
-  const title = 'Büyük Su';
-  const price = 20;
+  console.log('productsData: ', productsData);
+
+  console.log([11, 2, 3][0]);
 
   return (
     <div className="products">
       <h2>Products Component</h2>
 
       <div className="products-wrapper">
-        <ProductCard myImage={image} title={title} price={price} />
-        <ProductCard myImage={image} title={"Küçük Su"} price={10} />
-        <ProductCard myImage={image} title={title} price={price} />
-        <ProductCard myImage={image} title={title} price={price} />
-        <ProductCard myImage={image} title={title} price={price} />
-       
+        {productsData.map((product) => {
+          return (
+            <ProductCard
+              myImage={product.image}
+              title={product.title}
+              price={product.price}
+              desc={product.description}
+            />
+          );
+        })}
       </div>
     </div>
   );
