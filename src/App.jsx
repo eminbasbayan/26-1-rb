633ceebd43cdd7445ff7cc72554f607f880750cd
eminbasbayan <emin.basbayan@icloud.com>
2026-04-04 23:07:42 +0300

    useState hook counter ile kullanımı

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Fragment } from 'react';
 import Products from './components/Products/Products';
+import Counter from './components/Counter';
 
 function App() {
   return (
     <Fragment>
       <h1>App Component</h1>
+      <Counter />
       <Products />
    
     </Fragment>
