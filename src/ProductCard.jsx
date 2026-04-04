702fa3d944d38a4f13be555e886763991a1b5410
eminbasbayan <emin.basbayan@icloud.com>
2026-04-04 21:21:12 +0300

    componentlerde js degisken kullanımı

--- src/ProductCard.jsx
+++ src/ProductCard.jsx
@@ -1,16 +1,19 @@
 import './ProductCard.css';
 
 function ProductCard() {
+  const image =
+    'https://karatayberraksu.com.tr/wp-content/uploads/2019/03/5lt-buyuk.png';
+  const title = 'Büyük Su';
+  const price = 20000;
+
+  const toplam = price + 1000;
+
   return (
     <div className="product-card">
-      <img
-        className="product-image"
-        src="https://karatayberraksu.com.tr/wp-content/uploads/2019/03/5lt-buyuk.png"
-        alt="Image"
-      />
+      <img className="product-image" src={image} alt="Image" />
       <div className="product-info">
-        <strong className="product-name">Büyük Su</strong>
-        <span class="product-price">20₺</span>
+        <strong className="product-title">{title}</strong>
+        <span class="product-price">{toplam}₺</span>
       </div>
     </div>
   );
