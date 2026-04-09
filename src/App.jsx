4f790099f19707aaeb14f13a33bf6f49cc90ea26
eminbasbayan <emin.basbayan@icloud.com>
2026-04-09 22:42:32 +0300

    add product form component

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,11 @@
-import { Fragment, useState } from 'react';
+import { Fragment } from 'react';
 import Products from './components/Products/Products';
-import Counter from './components/Counter';
-import Button from './components/UI/Button';
 
 function App() {
   return (
     <Fragment>
       <h1>App Component</h1>
-      <Button title="1. Component" variant="primary" /> {/* 1. Component */}
-      <Button title="2. Component" variant="secondary" /> {/* 2. Component */}
-      <Button title="3. Component" variant="success" /> {/* 3. Component */}
-      <Button title="Sil" variant="danger" size="sm" />
-      <Button title="Ürün Ekle" variant="primary" size="lg" addClass="hidden" />
-      <Button variant="secondary" size="lg">
-        <b>Deneme Button</b>
-      </Button>
-      {/* <Counter /> */}
-      {/*    <button onClick={()=> setProductTitle("Kırmızı Ayakkabı")}>Başlığı Değiştir</button> {/* Kırmızı Ayakkabı */}
+
       <Products />
     </Fragment>
   );
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,14 +1,14 @@
 import ProductCard from './ProductCard.jsx';
 import productsData from '../../productsData.js';
 import './Products.css';
+import AddProductForm from './AddProductForm.jsx';
 
 // Ürünlerle ilgili ana parent component
 function Products() {
-
   return (
     <div className="products">
       <h2>Products Component</h2>
-
+      <AddProductForm />
       <div className="products-wrapper">
         {productsData.map((product) => {
           return (
