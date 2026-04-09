5d5d206270cf733e57a2141f1d0c1c06e16b5dab
eminbasbayan <emin.basbayan@icloud.com>
2026-04-09 22:56:12 +0300

    çoklu state ve çoklu function ile form yönetimi

--- src/components/Products/AddProductForm.jsx
+++ src/components/Products/AddProductForm.jsx
@@ -1,24 +1,62 @@
+import { useState } from 'react';
 import Button from '../UI/Button';
 import './AddProductForm.css';
 
 function AddProductForm() {
+  const [title, setTitle] = useState('');
+  const [image, setImage] = useState('');
+  const [price, setPrice] = useState('');
+  const [description, setDescription] = useState('');
+
+  function handleTitleChange(event) {
+    setTitle(event.target.value);
+  }
+
+  function handleImageChange(event) {
+    setImage(event.target.value);
+  }
+
+  function handlePriceChange(event) {
+    setPrice(event.target.value);
+  }
+
+  function handleDescriptionChange(event) {
+    setDescription(event.target.value);
+  }
+
   return (
     <form className="add-product-form">
       <label>
-        Title:
-        <input type="text" placeholder="Bir ürün ismi giriniz!" />
+        Title: {title}
+        <input
+          type="text"
+          placeholder="Bir ürün ismi giriniz!"
+          onChange={handleTitleChange}
+        />
       </label>
       <label>
-        Image URL:
-        <input type="text" placeholder="Bir ürün görseli giriniz!" />
+        Image URL: {image}
+        <input
+          type="text"
+          placeholder="Bir ürün görseli giriniz!"
+          onChange={handleImageChange}
+        />
       </label>
       <label>
-        Price:
-        <input type="number" placeholder="Bir ürün fiyatı giriniz!" />
+        Price: {price}
+        <input
+          type="number"
+          placeholder="Bir ürün fiyatı giriniz!"
+          onChange={handlePriceChange}
+        />
       </label>
       <label>
-        Description:
-        <input type="text" placeholder="Bir ürün açıklaması giriniz!" />
+        Description: {description}
+        <input
+          type="text"
+          placeholder="Bir ürün açıklaması giriniz!"
+          onChange={handleDescriptionChange}
+        />
       </label>
 
       <Button variant="primary">Ürünü Ekle</Button>
--- src/components/UI/Button.jsx
+++ src/components/UI/Button.jsx
@@ -1,8 +1,6 @@
 import './Button.css';
 
 function Button({ variant, size, title, addClass, children }) {
-  console.log(children);
-
   const classNames = `btn btn-${variant} btn-${size} ${addClass ? addClass : ''}`;
 
   return <button className={classNames}>{title ? title : children}</button>;
