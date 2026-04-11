ea43a8b486dfb48ce32c71505bec142ab77bd6eb
eminbasbayan <emin.basbayan@icloud.com>
2026-04-11 22:56:12 +0300

    modal component yapısı - lucide icon paketi

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Fragment } from 'react';
 import Products from './components/Products/Products';
+import Modal from './components/UI/Modal';
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <h1>App Component</h1>
 
       <Products />
+      <Modal />
     </Fragment>
   );
 }
