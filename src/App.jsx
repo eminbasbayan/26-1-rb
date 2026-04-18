d752d982b2fa5493a7f08399a4381c0b8ff263c7
eminbasbayan <emin.basbayan@icloud.com>
2026-04-18 22:51:17 +0300

    diğer sayfaların route'a eklenmesi

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,33 @@
-import { Fragment } from 'react';
-import Products from './components/Products/Products';
-import Counter from './components/Counter';
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
 import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import ProductsPage from './pages/ProductsPage';
+import RegisterPage from './pages/RegisterPage';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <HomePage />,
   },
+  {
+    path: "/products",
+    element: <ProductsPage />
+  },
+  {
+    path: "/login",
+    element: <LoginPage />
+  },
+  {
+    path: "/register",
+    element: <RegisterPage />
+  }
 ]);
 
 function App() {
   return (
     <div className='p-4'>
       <RouterProvider router={router} />
-      {/* <Counter /> */}
-      {/* <LoginPage /> */}
-      {/* <RegisterPage /> */}
-      {/*  <Products /> */}
     </div>
   );
 }
---
+++ src/pages/ProductsPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Products from '../components/Products/Products'
+
+const ProductsPage = () => {
+  return (
+    <div className='products-page'>
+        <h1>ProductsPage Component</h1>
+        <Products />
+    </div>
+  )
+}
+
+export default ProductsPage
