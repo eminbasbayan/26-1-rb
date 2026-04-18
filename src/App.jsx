b81b7a50d3209bdd7877cd4cff87dd5a757417bc
eminbasbayan <emin.basbayan@icloud.com>
2026-04-18 22:45:42 +0300

    router dom paketi kurulumu

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,25 @@ import Products from './components/Products/Products';
 import Counter from './components/Counter';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import HomePage from './pages/HomePage';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+]);
 
 function App() {
   return (
     <div className='p-4'>
-      <h1>App Component</h1>
+      <RouterProvider router={router} />
       {/* <Counter /> */}
       {/* <LoginPage /> */}
-      <RegisterPage />
-     {/*  <Products /> */}
+      {/* <RegisterPage /> */}
+      {/*  <Products /> */}
     </div>
   );
 }
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div className='home-page'>
+        <h1>HomePage Component</h1>
+    </div>
+  )
+}
+
+export default HomePage
